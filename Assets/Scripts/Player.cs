9a6a8b8d6357d9f9245eaec5f78e64febd9126ed
grzegorztomaszewski <aroxd3@gmail.com>
2019-01-22 14:17:43 +0100

    hp & mana Bar

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    [SerializeField]
+    private Stat health;
+
+    [SerializeField]
+    private Stat mana;
+
+
+    private float initHealth = 100;
+
+    private float initMana = 50;
+
+    protected override void Start()
+    {
+        health.Initialize(initHealth, initHealth);
+        mana.Initialize(initMana, initMana);
+
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        GetInput();
+        base.Update();
+    }
+
+
+    private void GetInput()
+    {
+        //TEST
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            health.MyCurrentValue -= 10;
+            mana.MyCurrentValue -= 10;
+        }
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            health.MyCurrentValue += 10;
+            mana.MyCurrentValue += 10;
+        }
+
+
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+    }
+}
